c55ffd4e795e2c4a7177b9f2a1cfe2bf3e89874c
User <asylzatwww@yandex.ru>
2019-02-27 12:56:09 +0300

    Initial commit

---
+++ app/src/main/java/com/cartoonworld/kg/hightime/MainActivity.java
@@ -0,0 +1,46 @@
+package com.cartoonworld.kg.hightime;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    /** Called when the user taps the Send button */
+    public void ONasActivityOpen(View view) {
+        Intent intent = new Intent(this, ONasActivity.class);
+        //EditText editText = (EditText) findViewById(R.id.editText);
+        //String message = editText.getText().toString();
+        //intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+    /** Called when the user taps the Send button */
+    public void StudentamActivityOpen(View view) {
+        Intent intent = new Intent(this, StudentamActivity.class);
+        //EditText editText = (EditText) findViewById(R.id.editText);
+        //String message = editText.getText().toString();
+        //intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+
+    /** Called when the user taps the Send button */
+    public void InstructionActivityOpen(View view) {
+        Intent intent = new Intent(this, InstructionActivity.class);
+        //EditText editText = (EditText) findViewById(R.id.editText);
+        //String message = editText.getText().toString();
+        //intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+
+}
---
+++ app/src/main/java/com/cartoonworld/kg/hightime/StudentamActivity.java
@@ -0,0 +1,13 @@
+package com.cartoonworld.kg.hightime;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StudentamActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_studentam);
+    }
+}
