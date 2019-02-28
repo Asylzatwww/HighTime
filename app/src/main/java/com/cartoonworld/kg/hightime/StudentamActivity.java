7b18083f70cdc01d53ba87aa2d4565f6c75f847a
User <asylzatwww@yandex.ru>
2019-02-28 09:03:09 +0300

    Test Activity Created

--- app/src/main/java/com/cartoonworld/kg/hightime/StudentamActivity.java
+++ app/src/main/java/com/cartoonworld/kg/hightime/StudentamActivity.java
@@ -1,7 +1,12 @@
 package com.cartoonworld.kg.hightime;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.cartoonworld.kg.hightime.test.Test1Activity;
+import com.cartoonworld.kg.hightime.test.Test2Activity;
 
 public class StudentamActivity extends AppCompatActivity {
 
@@ -10,4 +15,11 @@ public class StudentamActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_studentam);
     }
+
+    public void Test1ActivityOpen(View view) {
+        Intent intent = new Intent(this, Test1Activity.class);
+
+        startActivity(intent);
+    }
+
 }
